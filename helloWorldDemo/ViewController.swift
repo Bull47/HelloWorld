d347112a04f98b284ac83d80a0cf5a3c7759ad48
Ulf Henningsson <uffe@ulfs-mbp.lan>
2014-08-20 21:34:38 +0200

    Initial Commit

---
+++ helloWorldDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloWorldDemo
+//
+//  Created by Ulf Henningsson on 2014-08-20.
+//  Copyright (c) 2014 Ulf Henningsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
